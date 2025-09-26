97a3ac1808430f95ceccfb32888b84d1db17d070
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-09-26 22:35:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var name = "Peter king";
+
+var a = 20;
+var b = 30;
+
+// console.log(name);
+// console.log(a + b);
+
+
+console.log(global);
+console.log(this);  // Empty object
+
+console.log(globalThis === global);
+
+
