5b531e3b2423ea4b6a4b72f98e5d312d50e2721b
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-09-28 16:40:34 +0530

    Episode05

--- calculate/multiply.js
+++ calculate/multiply.js
@@ -1,7 +1,15 @@
+require("./xyz.js");
+
 function calculateMultiply (a, b) {
     const result = a + b;
 
     console.log(result);
 }
+
+setTimeout(() => {
+    console.log("setTimeout");
+    
+}, 3000);
+
 
 module.exports = { calculateMultiply };
--- calculate/xyz.js
+++ calculate/xyz.js
@@ -1,3 +1,24 @@
-console.log("very Important js code ");
+// require("./path")
+// Alll the code of the module is wrappedd inside a function (IIFE)
 
-var x = 100;
+console.log(__filename);
+console.log(__dirname);
+
+// // IIFE - Immediately Invoked function Expression
+
+// (function (module, require) {
+
+//     // All code of the module runs inside here
+//    function calculateMultiply (a, b) {
+//     const result = a + b;
+
+//     console.log(result);
+// }
+
+// module.exports = { calculateMultiply };
+
+// })();
+
+// (function (exports, require, module, __filename, __dirname) {
+//     // all the code of your module
+// })
