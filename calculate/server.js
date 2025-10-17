e72fdf1b3153091f30ac2eaf6201e256dcfeb0ef
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-10-17 23:45:04 +0530

    node .js-11

---
+++ calculate/server.js
@@ -0,0 +1,11 @@
+const http = require("node:http");
+
+
+const Server = http.createServer(function (req, res) {
+  if(req.url === "/getSecretData") {
+    res.end("There is no secret data");
+  }
+    res.end("Hello World!");
+});
+
+Server.listen(7777);
